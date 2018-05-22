05afa036252097e603e596298cbc61f52f953430
ay27 <jinmian.y@gmail.com>
2018-05-22 16:21:30 +0800

    add test for reader

--- reader.py
+++ reader.py
@@ -3,11 +3,13 @@
 # Created by ay27 at 2018/5/21
 import tensorflow as tf
 import numpy as np
+import utils
+utils.set_best_gpu()
 
 W = np.random.rand(20, 100) * 10.0
 
 
-def read_from_numpy(num_epochs, batch_size, name=None):
+def read_from_numpy(num_epochs, batch_size, shuffle=True, name=None):
     # refer to https://www.tensorflow.org/programmers_guide/datasets for more detail!
 
     num_examples = 1000
@@ -25,7 +27,8 @@ def read_from_numpy(num_epochs, batch_size, name=None):
         dataset = dataset.map(
             lambda x, y: (x + tf.random_uniform(x.get_shape(), minval=0, maxval=0.02, dtype=tf.float32), y),
             num_parallel_calls=4)
-        dataset.shuffle(buffer_size=1000)
+        if shuffle:
+            dataset.shuffle(buffer_size=1000)
         dataset = dataset.batch(batch_size)
         dataset = dataset.repeat(num_epochs)
         dataset = dataset.prefetch(buffer_size=1000)
@@ -36,7 +39,7 @@ def read_from_numpy(num_epochs, batch_size, name=None):
     return x_batch, y_batch, num_examples // batch_size + (0 if num_examples % batch_size == 0 else 1)
 
 
-def read_from_generator(num_epochs, batch_size, name=None):
+def read_from_generator(num_epochs, batch_size, shuffle=False, name=None):
     # refer to https://www.tensorflow.org/programmers_guide/datasets for more detail!
 
     num_examples = 1000
@@ -56,7 +59,8 @@ def read_from_generator(num_epochs, batch_size, name=None):
         dataset = dataset.map(
             lambda x, y: (x + tf.random_uniform(tf.shape(x), minval=0, maxval=0.02, dtype=tf.float32), y),
             num_parallel_calls=4)
-        dataset.shuffle(buffer_size=1000)
+        if shuffle:
+            dataset.shuffle(buffer_size=1000)
         dataset = dataset.batch(batch_size)
         dataset = dataset.repeat(num_epochs)
         dataset = dataset.prefetch(buffer_size=1000)
@@ -65,3 +69,16 @@ def read_from_generator(num_epochs, batch_size, name=None):
         x_batch, y_batch = iterator.get_next()
 
     return x_batch, y_batch, num_examples // batch_size + (0 if num_examples % batch_size == 0 else 1)
+
+
+if __name__ == '__main__':
+    # test reader
+    X, Y, iterations = read_from_numpy(1, 32, name="reader")
+    sess = tf.Session()
+    for ii in range(iterations):
+        x, y, = sess.run([X, Y])
+        print(ii, x.shape, y.shape)
+    try:
+        sess.run([X, Y])
+    except tf.errors.OutOfRangeError:
+        print("will catch an Error here!")
